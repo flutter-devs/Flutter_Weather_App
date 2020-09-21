0a1f7f7bc34bda080b04ea606cd3f130f3212435
anubhav11803451 <anubhav.11803451@lpu.in>
2020-09-21 20:10:00 +0530

    Update rooms.dart

--- roome/lib/src/screen/rooms.dart
+++ roome/lib/src/screen/rooms.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:roome/src/widgets/button.dart';
-import 'package:roome/src/widgets/customcardb.dart';
 
 class Rooms extends StatelessWidget {
   @override
