d0789d8e67b7b4480ad3e8f4fdda700f44529477
anubhav11803451 <anubhav.11803451@lpu.in>
2020-10-16 14:31:33 +0530

    Initial Commit (weather_app)

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+    );
+  }
+}
