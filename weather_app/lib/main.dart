1db7c8ed7fec5edd1e4504c8cbbd03490753b019
anubhav11803451 <anubhav.11803451@lpu.in>
2020-10-17 11:39:37 +0530

    final commit

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weather_app/screens/splahScreen.dart';
+import 'package:weather_app/theme/theme.dart';
 
 void main() {
   runApp(WeatherApp());
@@ -8,8 +11,13 @@ class WeatherApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: lighttheme(),
+      darkTheme: darktheme(),
+      themeMode: ThemeMode.system,
+      home: Splashscreen(),
     );
   }
 }
---
+++ weather_app/lib/screens/splahScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/animations/tweenAnimation.dart';
+
+class Splashscreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Opacityanimate(),
+    );
+  }
+}
